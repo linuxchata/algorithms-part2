0a20413b973f82942ba4176e77eeb62cd0824669
linuxchata <linuxchata@gmail.com>
2023-03-14 11:03:09 +0100

    Connected components algorithm

---
+++ W1.UndirectedGraphs/ConnectedComponents.cs
@@ -0,0 +1,49 @@
+﻿namespace W1.UndirectedGraphs;
+
+/// <summary>
+/// Connected Components data structure
+/// </summary>
+public class ConnectedComponents
+{
+    private readonly bool[] _marked;
+    private readonly int[] _cc;
+    private int _count;
+
+    public ConnectedComponents(Graph g)
+    {
+        _marked = new bool[g.GetSize()];
+        _cc = new int[g.GetSize()];
+
+        for (int v = 0; v < g.GetSize(); v++)
+        {
+            if (!_marked[v])
+            {
+                Dfp(g, v);
+                _count++;
+            }
+        }
+    }
+
+    public int Count()
+    {
+        return _count;
+    }
+
+    public int Cc(int v)
+    {
+        return _cc[v];
+    }
+
+    private void Dfp(Graph g, int v)
+    {
+        _marked[v] = true;
+        _cc[v] = _count;
+        foreach (int w in g.GetAdjacent(v))
+        {
+            if (!_marked[w])
+            {
+                Dfp(g, w);
+            }
+        }
+    }
+}
--- W1.UndirectedGraphs/Program.cs
+++ W1.UndirectedGraphs/Program.cs
@@ -5,7 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         // Graph
-        var graph = new Graph(12);
+        var graph = new Graph(13);
         graph.AddEdge(0, 6);
         graph.AddEdge(0, 1);
         graph.AddEdge(0, 2);
@@ -13,6 +13,11 @@ public class Program
         graph.AddEdge(6, 4);
         graph.AddEdge(4, 3);
         graph.AddEdge(4, 5);
+        graph.AddEdge(9, 12);
+        graph.AddEdge(11, 12);
+        graph.AddEdge(9, 10);
+        graph.AddEdge(7, 8);
+        graph.AddEdge(9, 11);
 
         var s = 0;
 
@@ -32,6 +37,9 @@ public class Program
 
         // Breadth-First Paths
         var bfp = new BreadthFirstPaths(graph, s);
+
+        // Connected Components
+        var cc = new ConnectedComponents(graph);
 
         Console.ReadKey();
     }

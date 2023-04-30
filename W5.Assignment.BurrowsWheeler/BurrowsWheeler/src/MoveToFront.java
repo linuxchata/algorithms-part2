e8cb820e36fd8385cd0d2af829ba2424482138f7
linuxchata <linuxchata@gmail.com>
2023-04-30 13:40:09 +0200

    Burrows–Wheeler assignment

--- W5.Assignment.BurrowsWheeler/BurrowsWheeler/src/MoveToFront.java
+++ W5.Assignment.BurrowsWheeler/BurrowsWheeler/src/MoveToFront.java
@@ -10,26 +10,81 @@
  *
  *----------------------------------------------------------------*/
 
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class MoveToFront {
+    private static final int R = 256;
+    private static final int LG_R = 8;
+
     /*
     Apply move-to-front encoding, reading from standard input and writing to standard output
      */
-    public static void encode(){
+    public static void encode() {
+        var table = getTable();
 
+        while (!BinaryStdIn.isEmpty()) {
+            var c = BinaryStdIn.readChar();
+            for (var j = 0; j < R; j++) {
+                if (table[j] == c) {
+                    BinaryStdOut.write(j, LG_R);
+                    table = getShuffledTable(table, c, j);
+                    break;
+                }
+            }
+        }
+        BinaryStdOut.close();
     }
 
     /*
     Apply move-to-front decoding, reading from standard input and writing to standard output
      */
-    public static void decode(){
+    public static void decode() {
+        var table = getTable();
 
+        while (!BinaryStdIn.isEmpty()) {
+            var j = BinaryStdIn.readInt(LG_R);
+            var c = table[j];
+            BinaryStdOut.write(c);
+            table = getShuffledTable(table, c, j);
+        }
+        BinaryStdOut.close();
+    }
+
+    private static char[] getTable() {
+        var table = new char[R];
+        for (var i = 0; i < R; i++) {
+            table[i] = (char) (i);
+        }
+        return table;
+    }
+
+    private static char[] getShuffledTable(char[] table, char c, int j) {
+        var newTable = new char[R];
+        newTable[0] = c;
+        System.arraycopy(table, 0, newTable, 1, j);
+        System.arraycopy(table, j + 1, newTable, j + 1, R - j - 1);
+        return newTable;
     }
 
     /*
     If args[0] is "-", apply move-to-front encoding
     If args[0] is "+", apply move-to-front decoding
      */
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws FileNotFoundException {
+        var fileName = args[2];
+        var fileInputStream = new FileInputStream(fileName);
+        System.setIn(fileInputStream);
+        var op = args[0];
+        if (op.equals("-")) {
+            MoveToFront.encode();
+        } else if (op.equals("+")) {
+            MoveToFront.decode();
+        }
     }
 }
